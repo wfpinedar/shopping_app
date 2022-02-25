c878ec84399854d36fa65b403a0a3e941e07d73d
wfpinedar <wfpinedar@gmail.com>
2022-02-24 23:18:01 -0500

    add urls for aplication

---
+++ admin/shop/urls.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from django.urls import path
+from .views import ShipmentViewSet, PaymentViewSet, ProductViewSet, OrderViewSet 
+
+urlpatterns = [
+    path('shipment', ShipmentViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('shipment/<str:pk>', ShipmentViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    })),
+    path('payment', PaymentViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('payment/<str:pk>', PaymentViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    })),
+    path('product', ProductViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('product/<str:pk>', ProductViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    })),
+    path('order', OrderViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('order/<str:pk>', OrderViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    }))
+]
