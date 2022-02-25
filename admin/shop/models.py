86aac00c482d55002f8917236b8d189aa32c1096
wfpinedar <wfpinedar@gmail.com>
2022-02-24 23:15:42 -0500

    add for models

--- admin/shop/models.py
+++ admin/shop/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
